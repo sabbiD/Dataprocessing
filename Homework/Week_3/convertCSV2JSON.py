09a9eb4573ee03b4daa7ea19130724991898495e
sabbiD <36192972+sabbiD@users.noreply.github.com>
2018-04-28 14:42:01 +0200

    week 3 first

---
+++ Homework/Week_3/convertCSV2JSON.py
@@ -0,0 +1,26 @@
+# Source: https://stackoverflow.com/questions/19697846/how-to-convert-csv-file-to-multiline-json
+
+import csv
+import json
+
+file = 'fietsen.csv'
+json_file = 'fietsen.json'
+
+#Read CSV File
+def read_CSV(file, json_file):
+    csv_rows = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        field = reader.fieldnames
+        for row in reader:
+            csv_rows.extend([{field[i]:row[field[i]] for i in range(len(field))}])
+        convert_write_json(csv_rows, json_file)
+
+#Convert csv data into json
+def convert_write_json(data, json_file):
+    with open(json_file, "w") as f:
+        f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ':')))
+        f.write(json.dumps(data))
+
+
+read_CSV(file,json_file)
