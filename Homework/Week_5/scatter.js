9cb3b58e100131cff479c8b9f6318ccecbc53400
sabbiD <36192972+sabbiD@users.noreply.github.com>
2018-05-18 22:36:19 +0200

    everything week 5

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -2,26 +2,24 @@
  Data processing 2018
  Sebile Demirtas 
  10548270
+ This file contains drawing the scatter plot and calling the function to update both graphs.
  Sources:
  - https://bl.ocks.org/d3noob/1ea51d03775b9650e8dfd03474e202fe (for easeBounce)
  - http://bl.ocks.org/schluppeck/b8745d4f4b98714a03a1da8e080d6a4e (for animations/transitions)
  - http://bl.ocks.org/WilliamQLiu/bd12f73d0b79d70bfbae (for animations/transitions)
  - http://alignedleft.com/tutorials/d3 (introduction to making scatterplot in d3)
+ - https://jnnnnn.blogspot.nl/2017/02/distinct-colours-2.html (for color scale with more than 20 colours)
  */
+window.onload = function(){
 
-//window.onload = function(){
-  
   // OECD API to load dataset
-  var researchWB = "researchDataWorldBank.json"
-  var patentsWB= "patentDataWorldBank.json"
-  
-  // declare empty list for coordinates 
-  var research60 = [] //G_XGDP GERD as a percentage of GDP
-  var patents60 = [] //P_PCT
-  var total60 = []
+  var researchWB = "researchDataWorldBank.json";
+  var patentsWB= "patentDataWorldBank.json";
 
-  // declare empty list for country Ids
-  var countryNames = []
+  // declare empty list for coordinates 
+  var research60 = [] 
+  var patents60 = [] 
+  var total60 = []
 
   // size of scatters
   var scatterSize = 5;
@@ -33,35 +31,33 @@
 
 
   function old_callback(error, data){
-    
+    if (error) throw error;
+      
     var patents = data[0]
     var research = data[1]
 
-    // check if data loads correctly
-    if (data.lenght == 0){
-      throw error;
-    } 
-    
     // collect datapoints for 267 countries
     for (var i = 3; i < 267; i++){
       
-
+      // dictionary with country name and values 
       total60.push({
         
         key: patents[i].FIELD1,
-        value: [patents[i].FIELD60 , research[i].FIELD60]
+        value: [research[i].FIELD60, patents[i].FIELD60]
 
       })
 
+      // list with values on x axis
       research60.push(research[i].FIELD60)
-      patents60.push(patents[i].FIELD60)
 
+      // list with values for y axis adjusted for scale
+      patents60.push((patents[i].FIELD60 / 1000))
     }
 
 
     // define dimensions
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
+    width = 550 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
     // define range x 
@@ -73,17 +69,23 @@
         .range([height, 0]);
 
     // colorscale for scatters and legend
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
+    var color = d3.scaleOrdinal(["#5776a9", "#678007", "#fa9316", "#85c070", "#6aa2a9", 
+            "#989e5d", "#fe9169", "#cd714a", "#6ed014", "#c5639c", "#c23271", "#698ffc",
+            "#678275", "#c5a121", "#a978ba", "#ee534e", "#d24506", "#59c3fa", "#ca7b0a", 
+            "#6f7385", "#9a634a", "#48aa6f", "#ad9ad0", "#d7908c", "#6a8a53", "#8c46fc", 
+            "#8f5ab8", "#fd1105", "#7ea7cf", "#d77cd1"]);
 
     // define X axis with range
-    var xAxis = d3.axisBottom(x);
+    var xAxis = d3.axisBottom(x)
+                  .ticks(5);
 
     // define Y axis with range 
-    var yAxis = d3.axisLeft(y);
+    var yAxis = d3.axisLeft(y)
+                  .ticks(3);
 
     // domain for X and Y axis
-    x.domain(d3.extent(research60, function(d) { return d; })).nice();
-    y.domain(d3.extent(patents60, function(d) { return d/1000; })).nice();
+    x.domain([0, d3.max(research60, function (d) { return d + 0.5; })]).nice();
+    y.domain([0, d3.max(patents60, function (d) { return d + 1000; })]).nice();
 
     // create SVG element
     var plot = d3.select("#scatter")
@@ -94,28 +96,38 @@
     	.append("g")
     	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
-    
+    // updating scatterplot
     function quickUpdate(){
 
-    plot.selectAll("circle")
-      .remove()
+      // remove old dots
+      plot.selectAll("circle")
+        .remove();
 
-    plot.selectAll(".legend")
-    .remove()
+      // remove old legend
+      plot.selectAll(".legend")
+        .remove();
 
-    countryNames = []
+      // initialise list to fill with country names in current continent
+      var countryNames = []
 
-    // add scatters to plot only if country is in continent
-    plot.selectAll("circle")
-      .data(total60)
-    .enter().append("circle")
+      
+      // add scatters to plot only if country is in continent
+      plot.selectAll("circle")
+        .data(total60)
+      .enter().append("circle")
+      .transition()
+      .duration(1000)
+      .ease(d3.easeBounce)
+      .on("start", function() {
+          d3.select(this) 
+            })
       .attr("cx", function(d) {
-          var xCoord = d.value[1]
+          var xCoord = d.value[0]
 
           return x(xCoord);
       })
       .attr("cy", function(d) {
-          var yCoord =  d.value[0]
+          var yCoord =  d.value[1]
 
           return y(yCoord);
       })
@@ -131,12 +143,11 @@
         }
         
         else {
-        return 0;
-        }
+        return 0;}
         })
       .style("fill", function(d, i){
         
-        if (d.value[0] === null || d.value[1] === null){
+          if (d.value[0] === null || d.value[1] === null){
           return 0;
         }
 
@@ -147,102 +158,101 @@
         else {
         return 0;}
         })
+        .on("end", function() {
+            d3.select(this)
+                .transition()
+                .duration(500)
+        });
     
-    var namesLegend = d3.scaleOrdinal(countryNames)
+        // list with country names in specific scatter plot
+        var namesLegend = d3.scaleOrdinal(countryNames);
 
-    // create X axis
-    plot.append("g")
-    	.attr("class", "x axis")
-    	.attr("transform", "translate(0," + height + ")")
-    	.call(xAxis);
+        // create X axis
+        plot.append("g")
+        	.attr("class", "x axis")
+        	.attr("transform", "translate(0," + height + ")")
+        	.call(xAxis);
 
-    // create Y axis
-    plot.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
+        // create Y axis
+        plot.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
 
-    // add label to Y axis
-    plot.append("text")
-    	.attr("class", "label")
-    	.attr("y", 6)
-      .attr("dy", ".71em")
-      .attr("transform", "rotate(-90)")
-    	.style("text-anchor", "end")
-    	.text("Patent applications x 1000");
-    
-    // add label to X axis
-    plot.append("text")
-    	.attr("class", "label")
-      .attr("x", width)
-    	.attr("y", height - 6)
-      .style("text-anchor", "end")
-    	.text("GERD as % of GDP");
+        // add label to Y axis
+        plot.append("text")
+        	.attr("class", "label")
+        	.attr("y", 6)
+          .attr("dy", ".71em")
+          .attr("transform", "rotate(-90)")
+        	.style("text-anchor", "end")
+        	.text("Patent applications x 1000");
+        
+        // add label to X axis
+        plot.append("text")
+        	.attr("class", "label")
+          .attr("x", width)
+        	.attr("y", height - 6)
+          .style("text-anchor", "end")
+        	.text("GERD as % of GDP");
 
-    // call legend 
-    var legend = plot.selectAll("legend")
-      .data(countryNames)
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d,i){ return "translate(0," + i * 10 + " )"})
+        // call legend 
+        var legend = plot.selectAll("legend")
+          .data(countryNames)
+        .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d,i){ return "translate(0," + i * 12 + " )"});
 
-    // add squares to legend
-    legend.append("rect")
-      .attr("x", width)
-      .attr("width", 9)
-      .attr("height", 9)
-      .style("fill", function(d){ return color(d)})
+        // add squares to legend
+        legend.append("rect")
+          .attr("x", width)
+          .attr("width", 10)
+          .attr("height", 10)
+          .style("fill", function(d){ return color(d)});
 
-    // add country ids to legend
-    legend.append("text")
-    .data(countryNames)
-      .attr("class", "label")
-      .attr("x", width - 6)
-      .attr("y", 1)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d){ 
-        return namesLegend(d);
-        })
+        // add country ids to legend
+        legend.append("text")
+        .data(countryNames)
+          .attr("class", "label")
+          .attr("x", width - 10)
+          .attr("y", 4)
+          .attr("dy", ".35em")
+          .style("text-anchor", "end")
+          .text(function(d){ 
+                  return namesLegend(d);
+            })
 
-    // add function to legend 
-    // when clicked on squares corresponding 
-    // scatter gets bigger
-    legend.on("click", function(type){
-      d3.selectAll("circle")
-          .style("opacity", 0.35)
-          .filter(function(d){
-            return d.key == type;
-          })
-          .style("opacity", 1)
-          .attr("r", r * 2)
+        // add function to legend 
+        // when clicked on squares corresponding 
+        legend.on("click", function(type){
+          d3.selectAll("circle")
+              .style("opacity", 0.35)
+              .filter(function(d){
+                return d.key == type;
+              })
+              .style("opacity", 1)
 
-        })
-}
-    
+            })
+    }
+      
     // draw scatter first time
     quickUpdate();
 
     // function to update both graphs 
     function changeBoth(){
 
-        var continent = this.getAttribute('id');
+      // get id of button to choose correct continent
+      var continent = this.getAttribute('id');
 
-        changeMap(continent);
-        quickUpdate();
+      changeMap(continent);
+      quickUpdate();
 
-      } 
+    } 
 
-      // linking buttons to update correct maps
-      document.getElementById("europe").onclick=changeBoth
-      document.getElementById("africa").onclick=changeBoth
-      document.getElementById("asia").onclick=changeBoth
-      document.getElementById("southAmerica").onclick=changeBoth
-      document.getElementById("northAmerica").onclick=changeBoth
-      document.getElementById("world").onclick=changeBoth
-
+    // linking buttons to update correct maps
+    document.getElementById("europe").onclick=changeBoth
+    document.getElementById("africa").onclick=changeBoth
+    document.getElementById("asia").onclick=changeBoth
+    document.getElementById("southAmerica").onclick=changeBoth
+    document.getElementById("northAmerica").onclick=changeBoth
   }
-
-  
-
-  
-//};
+}
--- Homework/Week_5/views.js
+++ Homework/Week_5/views.js
@@ -2,182 +2,192 @@
  Data processing 2018
  Sebile Demirtas 
  10548270
+ In this file the continent maps are loaded and drawn. Also tooltips for countries are added.
  Sources:
- -http://bl.ocks.org/MaciejKus/61e9ff1591355b00c1c1caf31e76a668(tooltips and zoom)
+ -http://bl.ocks.org/MaciejKus/61e9ff1591355b00c1c1caf31e76a668(tooltips)
  -https://bl.ocks.org/MariellaCC/0055298b94fcf2c16940
  -https://bl.ocks.org/mbostock/3306362(chloropeth map)
  */
-
-//window.onload = function(){
 	
-	// find datasets
-	var countriesGeo = []//new Set();
-	var africa = "Africa.json"
-	var asia = "Asia.json"
-	var southAmerica = "SouthAmerica.json"
-	var northAmerica = "NorthAmerica.json"
-	var europe = "Europe.json"
-	var oceania = "Oceania.json"
-	var world = "World.json"
-	var changeMap;
+// find datasets
+var africa = "Africa.json"
+var asia = "Asia.json"
+var southAmerica = "SouthAmerica.json"
+var northAmerica = "NorthAmerica.json"
+var europe = "Europe.json"
+var oceania = "Oceania.json"
 
-	d3.queue()
-	  .defer(d3.request, southAmerica)
-	  .defer(d3.request, northAmerica)
-	  .defer(d3.request, africa)
-	  .defer(d3.request, europe)
-	  .defer(d3.request, asia)
-	  .defer(d3.request, oceania)
-	  .defer(d3.request, world)
-	  .awaitAll(callback);
+// declaring global variables to be able to call later
+var changeMap;
+var countriesGeo = []
+
+d3.queue()
+  .defer(d3.request, southAmerica)
+  .defer(d3.request, northAmerica)
+  .defer(d3.request, africa)
+  .defer(d3.request, europe)
+  .defer(d3.request, asia)
+  .defer(d3.request, oceania)
+  .awaitAll(callback);
 
 
-	// define dimensions
-	var width = 400
-	var height = 300 
+// define dimensions
+var width = 500
+var height = 400 
+
+function callback(error, response) {
+	if (error) throw error;
+
+	// parse json files
+	var southAmerica = JSON.parse(response[0].responseText)
+	var northAmerica = JSON.parse(response[1].responseText)
+	var africa = JSON.parse(response[2].responseText)
+	var europe = JSON.parse(response[3].responseText)
+	var asia = JSON.parse(response[4].responseText)
+	var oceania = JSON.parse(response[5].responseText)
+
+	// dict with json files as values to use in changeBoth function
+	var continents = []
+
+	// fill dictionary for continents
+	continents["asia"] = asia
+	continents["africa"] = africa
+	continents["southAmerica"] = southAmerica
+	continents["northAmerica"] = northAmerica
+	continents["europe"] = europe
+	continents["oceania"] = oceania
+
+	// color scale map 
+	var legendColor = d3.scaleOrdinal()
+	.domain(["High", "High (non-OECD)", "Upper Middle", "Lower Middle", "Low"])
+	.range(["#f0f9e8" ,"#bae4bc" ,"#7bccc4", "#43a2ca", "#0868ac"].reverse())
+
+	// color scale map 
+	var mapColor = d3.scaleOrdinal()
+	.domain([1, 2, 3, 4, 5])
+	.range(["#f0f9e8" ,"#bae4bc" ,"#7bccc4", "#43a2ca", "#0868ac"].reverse())
+
+	// initialize placing for tooltips
+	var offsetL = document.getElementById('container').offsetLeft+10;
+	var offsetT = document.getElementById('container').offsetTop+10;
+
+	// initialize tooltips
+	var tooltip = d3.select("#container")
+		.append("div")
+		.attr("class", "tooltip hidden")
+
+	// define map projection
+	var projection = d3.geoMercator()
+
+	var path = d3.geoPath()
+				.projection(projection)
+
+	var svg = d3.select("#container")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height);
+		
+	var ordinal = d3.scaleOrdinal()
+	  .domain(["1", "2", "3", "4", "5"])
+	  .range(["#f0f9e8" ,"#bae4bc" ,"#7bccc4", "#43a2ca", "#0868ac"].reverse()); 
+
+	var svg = d3.select("svg");
+
+	svg.append("g")
+	  .attr("class", "legendOrdinal")
+	  .attr("transform", "translate(10,320)");
+
+	var legendOrdinal = d3.legendColor()
+		.title("Income categories (OECD): High to low.")
+		.titleWidth(150)
+	  .shapeWidth(40)
+	  .orient('horizontal')
+	  .scale(ordinal);
+
+	svg.select(".legendOrdinal")
+	  .call(legendOrdinal);
+
+
+	// tooltips with info on map
+	function showTooltip(d) {
+
+	  // info for tooltips
+      label = d.properties.name;
+      gdp = d.properties.gdp_md_est;
+      income_grp = d.properties.income_grp;
+      
+      // define content of tooltips
+      var mouse = d3.mouse(svg.node())
+        .map( function(d) { return parseInt(d); } );
+      	 tooltip.classed("hidden", false)
+        .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+        .html(label + "</br>" + income_grp)
+		}
 	
-	function callback(error, response) {
-		if (error) throw error;
 
-			// define dimensions
-	var width = 400
-	var height = 300
+	// setting scales accoring to json file of continent, asia
+	projection.fitSize([width, height], asia);
 
-		var southAmerica = JSON.parse(response[0].responseText)
-		var northAmerica = JSON.parse(response[1].responseText)
-		var africa = JSON.parse(response[2].responseText)
-		var europe = JSON.parse(response[3].responseText)
-		var asia = JSON.parse(response[4].responseText)
-		var oceania = JSON.parse(response[5].responseText)
-		var world = JSON.parse(response[6].responseText)
+	// fill list with countries in geo.json file for asia
+	for (var i = 0; i < asia.features.length; i++){
 
-		// dict with json files as values to use in update function
-		var continents = []
+		countriesGeo.push(asia.features[i].properties.admin)
 
-		continents["asia"] = asia
-		continents["africa"] = africa
-		continents["southAmerica"] = southAmerica
-		continents["northAmerica"] = northAmerica
-		continents["europe"] = europe
-		continents["oceania"] = oceania
-		continents["world"] = world
+	}
 
-		// color scale map 
-		var legendColor = d3.scaleOrdinal()
-		.domain(["High", "High (non-OECD)", "Upper Middle", "Lower Middle", "Low"])
-		.range(["#f0f9e8" ,"#bae4bc" ,"#7bccc4", "#43a2ca", "#0868ac"].reverse())
-
-		// color scale map 
-		var mapColor = d3.scaleOrdinal()
-		.domain([1, 2, 3, 4, 5])
-		.range(["#f0f9e8" ,"#bae4bc" ,"#7bccc4", "#43a2ca", "#0868ac"].reverse())
-
-		// initialize placing for tooltips
-		var offsetL = document.getElementById('container').offsetLeft+10;
-		var offsetT = document.getElementById('container').offsetTop+10;
-
-		// initialize tooltips
-		var tooltip = d3.select("#container")
-			.append("div")
-			.attr("class", "tooltip hidden")
-
-		// define map projection
-		var projection = d3.geoMercator()
-
-		var path = d3.geoPath()
-					.projection(projection)
-
-		var svg = d3.select("#container")
-				.append("svg")
-				.attr("width", width)
-				.attr("height", height);
+	// add countries to map with country name as id
+	// calling tooltips on hover
+	// adding on click function to select scatters
+	svg.selectAll("path")
+		.data(asia.features)
+		.enter()
+		.append("path")
+		.attr("d", path)
+		.attr("id", function(d) { return d.properties.admin })
+		.attr("stroke", "grey")
+		.attr("fill", function(d){ return mapColor(d.properties.income_grp[0]);})
+			.on("mousemove", showTooltip)
+  		.on("mouseout",  function(d,i) {
+      	tooltip.classed("hidden", true);
+   		})
+   		.on("click", function(type){
+	  			d3.selectAll("circle")
+	      		.style("opacity", 0.35)
+	      		.filter(function(d){
+	        	return d.key == type.properties.admin;
+	      		})
+	      		.style("opacity", 1)
+	      	})
   		
-  		var ordinal = d3.scaleOrdinal()
-		  .domain(["1", "2", "3", "4", "5"])
-		  .range(["#f0f9e8" ,"#bae4bc" ,"#7bccc4", "#43a2ca", "#0868ac"]); 
-
-		var svg = d3.select("svg");
-
-		svg.append("g")
-		  .attr("class", "legendOrdinal")
-		  .attr("transform", "translate(20,250)");
-
-		var legendOrdinal = d3.legendColor()
-		  .shapeWidth(30)
-		  .orient('horizontal')
-		  .scale(ordinal);
-
-		svg.select(".legendOrdinal")
-		  .call(legendOrdinal);
-
-
-		// tooltips with info on map
-		function showTooltip(d) {
-
-		  // info for tooltips
-	      label = d.properties.name;
-	      gdp = d.properties.gdp_md_est;
-	      income_grp = d.properties.income_grp;
-	      
-	      var mouse = d3.mouse(svg.node())
-	        .map( function(d) { return parseInt(d); } );
-	      	 tooltip.classed("hidden", false)
-	        .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
-	        .html(label + "</br>" + income_grp)
-			}
 		
 
-		// setting scales accoring to json file of continent
-		projection.fitSize([width, height], europe);
+	// changing map for continents
+  	function changeMapz(continent){
 
-		// list with countries in geo.json file
-		for (var i = 0; i < europe.features.length; i++){
+	  	// clearing svg for new map
+	  	svg.selectAll("path")
+	    .remove()
 
-			countriesGeo.push(europe.features[i].properties.admin)
-
-		}
-
-		// add countries to map with country name as class
-		svg.selectAll("path")
-			.data(europe.features)
-			.enter()
-			.append("path")
-			.attr("d", path)
-			.attr("id", function(d) { return d.properties.admin })
-			.attr("stroke", "grey")
-			.attr("fill", function(d){ return mapColor(d.properties.income_grp[0]);})
-  			.on("mousemove", showTooltip)
-      		.on("mouseout",  function(d,i) {
-          	tooltip.classed("hidden", true);
-       								})
-  		
-
-		// changing map for continents
-      	function changeMapz(continent){
-
-      	// clearing svg for new map
-      	svg.selectAll("path")
-        .remove()
-
-        // color scale map 
+	    // color scale map 
 		var mapColor = d3.scaleOrdinal()
 		.domain([1, 2, 3, 4, 5])
 		.range(["#f0f9e8" ,"#bae4bc" ,"#7bccc4", "#43a2ca", "#0868ac"].reverse())
 
-        // get name of continent to update map
-      	var json = continents[continent]
+	    // get name of continent to update map
+	  	var json = continents[continent]
 		
 		// setting scales according to json file of continent
 		projection.fitSize([width,height], json);
 		
 		// empty list with countries for new continent
 		countriesGeo = []
-			
-			for (var i = 0; i < json.features.length; i++){
+		
+		// adding to countriesGeo 
+		for (var i = 0; i < json.features.length; i++){
 
-				countriesGeo.push(json.features[i].properties.admin)
+			countriesGeo.push(json.features[i].properties.admin)
 
-			}
+		}
 			
 		// add countries to map with country name as class
 		svg.selectAll("path")
@@ -189,26 +199,24 @@
 			.attr("class", "country")
 			.attr("stroke", "grey")
 			.attr("fill", function(d){ return mapColor(d.properties.income_grp[0]);})
-  			.on("mousemove", showTooltip)
-      		.on("mouseout",  function(d,i) {
-          	tooltip.classed("hidden", true);
-           								})
-      		.on("click", function(type){
-      			d3.selectAll("circle")
-          		.style("opacity", 0.35)
-          		.filter(function(d){
-          			console.log(d.key, type.properties.admin)
-            	return d.key == type.properties.admin;
-          		})
-          		.style("opacity", 1)
-          	})
+				.on("mousemove", showTooltip)
+	  		.on("mouseout",  function(d,i) {
+	      	tooltip.classed("hidden", true);
+	       								})
+	  		.on("click", function(type){
+	  			d3.selectAll("circle")
+	      		.style("opacity", 0.35)
+	      		.filter(function(d){
+	        	return d.key == type.properties.admin;
+	      		})
+	      		.style("opacity", 1)
+	      	})
 
 	}
 
-  // making function global to be able to call later
-  changeMap = changeMapz;
+	// making function global to be able to call later
+	changeMap = changeMapz;
 }
-
 
 
 
